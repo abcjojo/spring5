7020b14a9ccaafe40027de6d8f0c4534f15d1811
abcjojo <497601974@qq.com>
2021-07-16 18:39:03 +0800

    a

---
+++ reacor4j8/src/main/java/com/atguigu/reacor4j8/Reacor4j8Application.java
@@ -0,0 +1,13 @@
+package com.atguigu.reacor4j8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reacor4j8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Reacor4j8Application.class, args);
+    }
+
+}
---
+++ reacor4j8/src/test/java/com/atguigu/reacor4j8/Reacor4j8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.reacor4j8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reacor4j8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
